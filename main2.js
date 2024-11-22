7e5a1e1ff0286f28c06fd95ad7aada96c741de65
Albincaz <albinca9207@gmail.com>
2024-11-22 08:54:14 +0530

    done

--- main2.js
+++ main2.js
@@ -1,13 +1,49 @@
 
-console.log("hi,there");
+// console.log("hi,there");
 
 
-setTimeout(() => {
-console.log("keerthi")   
-},5000);
+// setTimeout(() => {
+// console.log("keerthi")   
+// },5000);
 
-function great(){
-    console.log("bye");
+// function great(){
+//     console.log("bye");
+// }
+// setTimeout(great,10000);
+
+
+
+// class person{
+//     constructor(name,age){
+//         this.name=name;
+//         this.age=age;
+//     }
+//     greet(){
+//         console.log('Hi, Im'+this.name+'and Im '+this.age +'years old.');
+//     }
+// }
+
+// const person1=new person('bob',30);
+// person1.greet();
+
+
+
+class Animal{
+    constructor(name){
+        this.name=name;
+    }
+    speak(){
+        // console.log('${this.name} makes a noise');
+        console.log(this.name+' makes a noise');
+    }
 }
-setTimeout(great,10000);
-    
+class Dog extends Animal{
+    speak(){
+        // console.log('${this.name} barks');
+        console.log(this.name +' barks');
+
+    }
+}
+
+const dog=new Dog('Rex');
+dog.speak();
